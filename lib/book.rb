032be29cfe96b1c6489b37d74cfd947f017c6502
Abdul <awsayed@alaska.edu>
2019-06-06 05:26:04 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,23 @@
 
 class Book
-  def initialize(title)
+  def initialize(title, author)
     @title = title
+    @author = author
   end
-  
+
+  # Read the title
   def title
     @title
   end
+
+  # write an author
+  def author=(author)
+    @author = author
+  end
 end
+
+newBook = Book.new("Mobey Dick")
+
+puts newBook.title
 
 
